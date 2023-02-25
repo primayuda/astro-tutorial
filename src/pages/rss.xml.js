3f1a0268a81db9e032f59cb2135e92bcf226ac46
primayuda <aprita.primayuda@gmail.com>
2023-02-25 14:05:16 +0700

    Adding RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: 'Aprita Primayuda | Blog',
+    description: 'My Astro tutorial blog',
     site: 'https://prims-astro-tutorial.netlify.app/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
